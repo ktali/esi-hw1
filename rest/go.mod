844bc07d33fb47f650e099b670f83d4f2a90c547
kuusik <kuusik@ttu.ee>
2021-03-01 19:42:42 +0200

    setup main router paths

--- rest/go.mod
+++ rest/go.mod
@@ -1,3 +1,5 @@
 module github.com/ktali/esi-hw1/rest
 
 go 1.16
+
+require github.com/gorilla/mux v1.8.0
---
+++ rest/main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/gorilla/mux"
+	"log"
+	"net/http"
+)
+
+type Do struct {
+	Id      int    `json:"id"`
+	Content string `json:"content"`
+}
+
+type Dos []Do
+
+func listDos(w http.ResponseWriter, r *http.Request) {
+	dos := Dos{
+		Do{Id: 1, Content: "Test"},
+	}
+	json.NewEncoder(w).Encode(dos)
+}
+
+func addDos(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Not implemented")
+}
+
+func removeDos(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Not implemented")
+}
+
+func completeDos(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Not implemented")
+}
+
+func handleRequests() {
+	router := mux.NewRouter().StrictSlash(true)
+	router.HandleFunc("/", listDos).Methods("GET")
+	router.HandleFunc("/add", addDos).Methods("POST")
+	router.HandleFunc("/remove", removeDos).Methods("DELETE")
+	router.HandleFunc("/complete", completeDos).Methods("PUT")
+	log.Fatal(http.ListenAndServe(":8081", nil))
+}
+
+func main() {
+	handleRequests()
+}
